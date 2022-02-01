53ab3aa1a2d58752c7319f7e92a702ac5e3d3ea2
Matheus Guedes <matheusguedessz@gmail.com>
2022-02-01 08:51:08 -0300

    commit 01-02-22
    faltando a página de contatos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const _elementsHome = {
     navbar: document.querySelector('.navbar'),
     menuHamburguer: document.querySelector('.fa-bars'),
-
 }
+
+const _elementsCourse = {
+    course: document.querySelectorAll('.course')
+}
+
 _elementsHome.menuHamburguer.addEventListener('click', () =>{
    _elementsHome.navbar.classList.toggle('shownavbar')
    if(_elementsHome.menuHamburguer.classList.contains('fa-bars')){
@@ -12,4 +16,10 @@ _elementsHome.menuHamburguer.addEventListener('click', () =>{
     _elementsHome.menuHamburguer.classList.replace('fa-times', 'fa-bars')
   _elementsHome.menuHamburguer.style.color = 'rgb(21, 21, 100)'
    }
+})
+
+_elementsCourse.course.forEach(course =>{
+  course.addEventListener('click', ()=>{
+    window.location.href = "course-inner.html"
+  })
 })
